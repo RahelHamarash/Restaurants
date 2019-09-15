a0bfd9439ed799f4b43960482ecf7d30f97d5570
Rahel Hamarash <Rahell.Hamarash@gmail.com>
2019-09-15 13:14:02 +0300

    first commit

---
+++ src/pages/Content.js
@@ -0,0 +1,21 @@
+import React , {Component} from 'react' ;
+import {BrowserRouter as Router , Switch , Route } from 'react-router-dom' ;
+import Home from './Home' ;
+import Level from './Level' ;
+class Content extends Component {
+
+    render(){
+        
+        return (
+            
+            <Router>
+                <Switch>
+                    <Route exact path="/dashboard" component={Home}/>
+                    <Route path="/Level" component={Level}/>    
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default Content ;
---
+++ src/pages/Level.js
@@ -0,0 +1,14 @@
+import {React , Component} from 'react' ;
+
+class Level extends Component {
+
+    render (){
+
+        return (
+
+            <h1> Level </h1>
+        )
+    }
+}
+
+export default Level ; 
